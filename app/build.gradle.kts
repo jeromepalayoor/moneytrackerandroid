2fa3437c2125fec697f65121d031d011c176f3d4
jeromepalayoor <me@jp-ch.gq>
2026-06-12 23:35:06 +0800

    status bar colour fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "net.jpalayoor.moneytracker"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
